4ebd48f5d592c0fce47ec4c4ba281fa151da98d7
AlexToW <trishin.as@phystech.edu>
2022-01-09 15:03:05 +0300

    dot_w_vect задаём в ССК

--- my_rightSide.m
+++ my_rightSide.m
@@ -15,6 +15,6 @@ dot_w_vect = params.inv_J*(M_vect_BF - cross(w_vect_BF, params.J * w_vect_BF));
 
 dot_x(1:3) = V_vect;
 dot_x(4:6) = -mu/norm(R_vect).^3 * R_vect;
-dot_x(7:9) = IF2BF' * dot_w_vect;
+dot_x(7:9) = dot_w_vect;
 dot_x(10:13) = 0.5*quatmultiply(Q, [0, w_vect_BF']);
 end
